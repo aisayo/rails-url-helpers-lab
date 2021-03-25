8b2dcf90f119e9112d2956f307e29900a7637f9e
Aysan Isayo <aisayo@icloud.com>
2021-03-25 14:22:29 -0700

    lab walkthrough

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,34 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show]
   
   def index
     @students = Student.all
   end
 
   def show
+    
   end
+
+  def activate
+   # to change the active status of a student
+   # if active == false, true
+   # if active == true, false
+
+  #  if @student.active == false
+  #   @student.active = true
+  #  else 
+  #   @student.active = false 
+  #  end 
+  #  @student.save
+   @student.active = !@student.active 
+   @student.save
+   redirect_to student_path(@student)
+  end 
 
   private
 
     def set_student
       @student = Student.find(params[:id])
     end
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Student.create([{first_name: "yuliana", last_name: "a"}, {first_name: "amanda", last_name: "b"}, {first_name: "nico", last_name: "b"}, {first_name: "aiden", last_name: "m"}, {first_name: "sam" , last_name: "0"}])
